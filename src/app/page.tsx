58cedf140ca8f739bda8fc638ac9a14aac377d0a
AkramRamoul <ahmedakramramoul@gmail.com>
2025-06-30 19:55:31 +0100

    custom style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import { Button } from "@/components/ui/button";
-
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-400">Hello</p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant={"elevated"}>Button</Button>
+        </div>
+        <div>
+          <Input placeholder="Input"></Input>
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="Textarea"></Textarea>
+        </div>
+        <div>
+          <Checkbox></Checkbox>
+        </div>
+      </div>
     </div>
   );
 }
