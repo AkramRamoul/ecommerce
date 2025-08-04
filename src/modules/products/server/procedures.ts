aff6ec3fcbabbcc2f008e318ab9f12c7f107752c
AkramRamoul <ahmedakramramoul@gmail.com>
2025-08-04 20:10:25 +0100

    feat: refactor product listing components and implement infinite scrolling

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,14 +1,17 @@
-import { Category } from "@/payload-types";
+import { Category, Media } from "@/payload-types";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import type { Sort, Where } from "payload";
 
 import z from "zod";
 import { sortValues } from "../search-Params";
+import { PAGE_LIMIT } from "@/constants";
 
 export const productsRouter = createTRPCRouter({
   getMany: baseProcedure
     .input(
       z.object({
+        cursor: z.number().default(1),
+        limit: z.number().default(PAGE_LIMIT),
         categorySlug: z.string().nullable().optional(),
         minPrice: z.string().nullable().optional(),
         maxPrice: z.string().nullable().optional(),
@@ -47,7 +50,6 @@ export const productsRouter = createTRPCRouter({
       if (input.categorySlug) {
         const data = await ctx.payload.find({
           collection: "categories",
-          limit: 1,
           depth: 1,
           pagination: false,
           where: {
@@ -87,10 +89,18 @@ export const productsRouter = createTRPCRouter({
       const data = await ctx.payload.find({
         collection: "products",
         depth: 1,
+        page: input.cursor,
+        limit: input.limit,
         where,
         sort,
       });
 
-      return data;
+      return {
+        ...data,
+        docs: data.docs.map((doc) => ({
+          ...doc,
+          image: doc.image as Media | null,
+        })),
+      };
     }),
 });
---
+++ src/modules/products/ui/components/ProductCard.tsx
@@ -0,0 +1,84 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+interface ProductCardProps {
+  id: string;
+  name: string;
+  imageUrl?: string | null;
+  authorUsername: string;
+  authorImageUrl?: string | null;
+  reviewRating: number;
+  reviewCount: number;
+  price: number;
+}
+
+export const ProductCard = ({
+  id,
+  name,
+  imageUrl,
+  authorUsername,
+  authorImageUrl,
+  reviewRating,
+  reviewCount,
+  price,
+}: ProductCardProps) => {
+  return (
+    <Link href={`/products/${id}`} className="no-underline">
+      <div
+        className="border rounded-md bg-white overflow-hidden h-full flex flex-col
+      hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transtition-shadow"
+      >
+        <div className="relative aspect-square">
+          <Image
+            src={imageUrl || "/authbg.png"}
+            alt={name}
+            fill
+            className="object-cover"
+          />
+        </div>
+        <div className="p-4 flex flex-col border-y gap-3 flex-1">
+          <h2 className="text-lg font-medium line-clamp-4">{name}</h2>
+          <div className="flex items-center gap-2">
+            {authorImageUrl && (
+              <Image
+                src={authorImageUrl}
+                alt={authorUsername}
+                width={16}
+                height={16}
+                className="rounded-full border shrink-0 size-[16px]"
+              />
+            )}
+
+            <p className="text-sm font-medium underline">{authorUsername}</p>
+          </div>
+          {reviewCount > 0 && (
+            <div className="flex items-center gap-1">
+              <span className="text-yellow-500">
+                {"★"
+                  .repeat(reviewRating)
+                  .padEnd(5, "☆")
+                  .split("")
+                  .map((star, index) => (
+                    <span key={index}>{star}</span>
+                  ))}
+              </span>
+              <p className="text-sm font-medium">({reviewCount})</p>
+            </div>
+          )}
+        </div>
+        <div className="p-4 ">
+          <div className="relartive px-2 py-1 border bg-rose-400 w-fit">
+            <p className="text-sm font-medium">DA{price.toFixed(2)}</p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export const ProductCardSkeleton = () => {
+  return (
+    <div className="w-full aspect-3/4 bg-neutral-200 rounded-lg animate-pulse"></div>
+  );
+};
