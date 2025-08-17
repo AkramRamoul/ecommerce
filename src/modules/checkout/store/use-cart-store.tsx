c636fb81bf0b9ede89970ec537e045b0c094803b
AkramRamoul <ahmedakramramoul@gmail.com>
2025-08-17 20:38:36 +0100

    feat: add cart functionality with Zustand for tenant-based product management

---
+++ src/modules/checkout/store/use-cart-store.tsx
@@ -0,0 +1,68 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+interface Tenantcart {
+  productIds: string[];
+}
+
+interface CartState {
+  tenantCarts: Record<string, Tenantcart>;
+  addProduct: (tenantSlug: string, productId: string) => void;
+  removeProduct: (tenantSlug: string, productId: string) => void;
+  clearCart: (tenantSlug: string) => void;
+  clearAllCarts: () => void;
+  getCartByTenant: (tenantSlug: string) => string[];
+}
+
+export const useCartStore = create<CartState>()(
+  persist(
+    (set, get) => ({
+      tenantCarts: {},
+      addProduct: (tenantSlug, productId) =>
+        set((state) => ({
+          tenantCarts: {
+            ...state.tenantCarts,
+            [tenantSlug]: {
+              productIds: [
+                ...(state.tenantCarts[tenantSlug]?.productIds || []),
+                productId,
+              ],
+            },
+          },
+        })),
+
+      removeProduct: (tenantSlug, productId) =>
+        set((state) => ({
+          tenantCarts: {
+            ...state.tenantCarts,
+            [tenantSlug]: {
+              productIds:
+                state.tenantCarts[tenantSlug]?.productIds.filter(
+                  (id) => id !== productId
+                ) || [],
+            },
+          },
+        })),
+      clearCart: (tenantSlug) =>
+        set((state) => ({
+          tenantCarts: {
+            ...state.tenantCarts,
+            [tenantSlug]: {
+              productIds: [],
+            },
+          },
+        })),
+      clearAllCarts: () =>
+        set({
+          tenantCarts: {},
+        }),
+      getCartByTenant: (tenantSlug) =>
+        get().tenantCarts[tenantSlug]?.productIds || [],
+    }),
+
+    {
+      name: "multicart",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
