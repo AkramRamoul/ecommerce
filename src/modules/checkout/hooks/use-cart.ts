a5109e843bdb66cbea8f275a9eb5044c0b288727
AkramRamoul <ahmedakramramoul@gmail.com>
2025-09-13 19:49:18 +0100

    feat: refactor useCart hook and store to improve product management and state handling

--- src/modules/checkout/hooks/use-cart.ts
+++ src/modules/checkout/hooks/use-cart.ts
@@ -1,27 +1,33 @@
+import { useCallback } from "react";
 import { useCartStore } from "../store/use-cart-store";
-
+import { useShallow } from "zustand/react/shallow";
 export const useCart = (tenantSlug: string) => {
-  const {
-    addProduct,
-    removeProduct,
-    clearCart,
-    clearAllCarts,
-    getCartByTenant,
-  } = useCartStore();
+  const addProduct = useCartStore((state) => state.addProduct);
+  const removeProduct = useCartStore((state) => state.removeProduct);
+  const clearCart = useCartStore((state) => state.clearCart);
+  const clearAllCarts = useCartStore((state) => state.clearAllCarts);
 
-  const productIds = getCartByTenant(tenantSlug);
+  const productIds = useCartStore(
+    useShallow((state) => state.tenantCarts[tenantSlug]?.productIds || [])
+  );
 
-  const toggleProduct = (productId: string) => {
-    if (productIds.includes(productId)) {
-      removeProduct(tenantSlug, productId);
-    } else {
-      addProduct(tenantSlug, productId);
-    }
-  };
+  const toggleProduct = useCallback(
+    (productId: string) => {
+      if (productIds.includes(productId)) {
+        removeProduct(tenantSlug, productId);
+      } else {
+        addProduct(tenantSlug, productId);
+      }
+    },
+    [addProduct, removeProduct, productIds, tenantSlug]
+  );
 
-  const isProsuctInCart = (productId: string) => {
-    return productIds.includes(productId);
-  };
+  const isProsuctInCart = useCallback(
+    (productId: string) => {
+      return productIds.includes(productId);
+    },
+    [productIds]
+  );
 
   const clearTenantCart = () => {
     clearCart(tenantSlug);
